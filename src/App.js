b4dd0c2d77a42751052d0a6b9dce6695e3bcd382
miniquets <106190210+miniquets@users.noreply.github.com>
2022-05-26 17:21:45 -0700

    Completed first build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React, { Component } from "react";
+import Counter from "./Counter";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <div>Hello World!</div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+     
+        <hi> This is the blank state we're working with</hi>
+        <Counter />
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
